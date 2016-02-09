9f56d1fcf5148f147a8b17eee40ede1565c6fa57
Kain93 <seregamitayev.93@gmail.com>
2016-02-09 21:02:27 +0600

    Revert "	new file:   V4.ino"
    This reverts commit 488d3564e715ef8bfb8f02051359905f03b9017c.

--- charge.sh
+++
@@ -1,6 +0,0 @@
-echo "Charging" | sudo tee /dev/tty1
-./gpio.sh 17 0
-sleep 1.5
-./gpio.sh 17 1
-sleep 1
-echo "Charging complete" | sudo tee /dev/tty1
--- discharge.sh
+++
@@ -1,6 +0,0 @@
-echo "Discharging" | sudo tee /dev/tty1
-./gpio.sh 23 0& 
-sleep 10
-./gpio.sh 23 1&
-sleep 1
-echo "Discharging complete" | sudo tee /dev/tty1
